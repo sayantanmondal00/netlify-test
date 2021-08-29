329d3fd3006d93146b8b60afcce5805a9db0bab2
Sayantan Mondal <sayantanmondal00@gmail.com>
2021-08-29 21:18:12 +0530

    connected netlify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World!
+          Hello World and other planets!
         </a>
       </header>
     </div>
